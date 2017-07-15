31dc23f54cfe3d1a2334326fa2eeabdb46a3cf6d
gruijter <gruijter@hotmail.com>
2017-07-15 19:45:48 +0200

    Bugfixes makeRequest. Prepared for device blocking.

--- netgear.js
+++ netgear.js
@@ -7,7 +7,11 @@ const ACTION_LOGIN = 'urn:NETGEAR-ROUTER:service:ParentalControl:1#Authenticate'
 const ACTION_GET_ATTACHED_DEVICES = 'urn:NETGEAR-ROUTER:service:DeviceInfo:1#GetAttachDevice';
 const ACTION_GET_ATTACHED_DEVICES2 = 'urn:NETGEAR-ROUTER:service:DeviceInfo:1#GetAttachDevice2';
 const ACTION_GET_TRAFFIC_METER = 'urn:NETGEAR-ROUTER:service:DeviceConfig:1#GetTrafficMeterStatistics';
-const SESSION_ID = 'A7D88AE69687E58D9A00';
+const ACTION_CONFIGURATION_STARTED = 'urn:NETGEAR-ROUTER:service:DeviceConfig:1#ConfigurationStarted';
+const ACTION_CONFIGURATION_FINISHED = 'urn:NETGEAR-ROUTER:service:DeviceConfig:1#ConfigurationFinished';
+const ACTION_SET_BLOCK_DEVICE = 'urn:NETGEAR-ROUTER:service:DeviceConfig:1#SetBlockDeviceByMAC';
+
+const SESSION_ID = 'A7D88AE69687E58D9A00'; // '10588AE69687E58D9A00'
 
 const REGEX_ATTACHED_DEVICES = new RegExp('<NewAttachDevice>(.*)</NewAttachDevice>');
 const REGEX_NEW_TODAY_UPLOAD = new RegExp('<NewTodayUpload>(.*)</NewTodayUpload>');
@@ -34,19 +38,59 @@ function soapLogin(sessionId, username, password) {
 	  </SOAP-ENV:Envelope>`;
 }
 
+function soapConfigurationStarted(sessionId) {
+	return `<?xml version="1.0" encoding="utf-8" standalone="no"?>
+	<SOAP-ENV:Envelope xmlns:SOAPSDK1="http://www.w3.org/2001/XMLSchema" xmlns:SOAPSDK2="http://www.w3.org/2001/XMLSchema-instance" xmlns:SOAPSDK3="http://schemas.xmlsoap.org/soap/encoding/" xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/">
+		<SOAP-ENV:Header>
+			<SessionID>${sessionId}</SessionID>
+		</SOAP-ENV:Header>
+		<SOAP-ENV:Body>
+			<M1:ConfigurationStarted xmlns:M1="urn:NETGEAR-ROUTER:service:DeviceConfig:1">
+				<NewSessionID>${sessionId}</NewSessionID>
+			</M1:ConfigurationStarted>
+		</SOAP-ENV:Body>
+	</SOAP-ENV:Envelope>`;
+}
+
+function soapConfigurationFinished(sessionId) {
+	return `<?xml version="1.0" encoding="utf-8" standalone="no"?>
+		<SOAP-ENV:Envelope xmlns:SOAPSDK1="http://www.w3.org/2001/XMLSchema" xmlns:SOAPSDK2="http://www.w3.org/2001/XMLSchema-instance" xmlns:SOAPSDK3="http://schemas.xmlsoap.org/soap/encoding/" xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/">
+			<SOAP-ENV:Header>
+				<SessionID>${sessionId}</SessionID>
+			</SOAP-ENV:Header>
+			<SOAP-ENV:Body>
+				<M1:ConfigurationFinished xmlns:M1="urn:NETGEAR-ROUTER:service:DeviceConfig:1">
+					<NewStatus>ChangesApplied</NewStatus>
+				</M1:ConfigurationFinished>
+			</SOAP-ENV:Body>
+		</SOAP-ENV:Envelope>`;
+}
+
+function soapSetBlockDevice(sessionId, mac, AllowOrBlock) {
+	return `<?xml version="1.0" encoding="utf-8" standalone="no"?>
+		<SOAP-ENV:Envelope xmlns:SOAPSDK1="http://www.w3.org/2001/XMLSchema" xmlns:SOAPSDK2="http://www.w3.org/2001/XMLSchema-instance" xmlns:SOAPSDK3="http://schemas.xmlsoap.org/soap/encoding/" xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/">
+			<SOAP-ENV:Header>
+				<SessionID>${sessionId}</SessionID>
+			</SOAP-ENV:Header>
+			<SOAP-ENV:Body>
+				<M1:SetBlockDeviceByMAC xmlns:M1="urn:NETGEAR-ROUTER:service:DeviceConfig:1">
+					<NewAllowOrBlock>${AllowOrBlock}</NewAllowOrBlock>
+					<NewMACAddress>${mac}</NewMACAddress>
+				</M1:SetBlockDeviceByMAC>
+			</SOAP-ENV:Body>
+		</SOAP-ENV:Envelope>`;
+}
+
 function soapAttachedDevices(sessionId) {
-	return `<?xml version="1.0" encoding="utf-8" standalone="no"?> +
-    <SOAP-ENV:Envelope xmlns:SOAPSDK1="http://www.w3.org/2001/XMLSchema" +
-			xmlns:SOAPSDK2="http://www.w3.org/2001/XMLSchema-instance" +
-			xmlns:SOAPSDK3="http://schemas.xmlsoap.org/soap/encoding/" +
-			xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/"> +
-    <SOAP-ENV:Header> +
-    <SessionID>${sessionId}</SessionID> +
-    </SOAP-ENV:Header> +
-    <SOAP-ENV:Body> +
-    <M1:GetAttachDevice xmlns:M1="urn:NETGEAR-ROUTER:service:DeviceInfo:1"> +
-    </M1:GetAttachDevice> +
-    </SOAP-ENV:Body> +
+	return `<?xml version="1.0" encoding="utf-8" standalone="no"?>
+    <SOAP-ENV:Envelope xmlns:SOAPSDK1="http://www.w3.org/2001/XMLSchema" xmlns:SOAPSDK2="http://www.w3.org/2001/XMLSchema-instance"	xmlns:SOAPSDK3="http://schemas.xmlsoap.org/soap/encoding/" xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/">
+    <SOAP-ENV:Header>
+    <SessionID>${sessionId}</SessionID>
+    </SOAP-ENV:Header>
+    <SOAP-ENV:Body>
+    <M1:GetAttachDevice xmlns:M1="urn:NETGEAR-ROUTER:service:DeviceInfo:1">
+    </M1:GetAttachDevice>
+    </SOAP-ENV:Body>
     </SOAP-ENV:Envelope>`;
 }
 
@@ -63,19 +107,15 @@ function soapAttachedDevices2(sessionId) {
 	</SOAP-ENV:Envelope>`;
 }
 
-
 function soapTrafficMeter(sessionId) {
-	return `<?xml version="1.0" encoding="UTF-8" standalone="no"?> +
-		<SOAP-ENV:Envelope xmlns:SOAPSDK1="http://www.w3.org/2001/XMLSchema" +
-		  xmlns:SOAPSDK2="http://www.w3.org/2001/XMLSchema-instance" +
-		  xmlns:SOAPSDK3="http://schemas.xmlsoap.org/soap/encoding/" +
-		  xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/"> +
-		<SOAP-ENV:Header> +
-		<SessionID>${sessionId}</SessionID> +
-		</SOAP-ENV:Header> +
-		<SOAP-ENV:Body> +
-		<M1:GetTrafficMeterStatistics xmlns:M1="urn:NETGEAR-ROUTER:service:DeviceConfig:1"></M1:GetTrafficMeterStatistics> +
-		</SOAP-ENV:Body> +
+	return `<?xml version="1.0" encoding="UTF-8" standalone="no"?>
+		<SOAP-ENV:Envelope xmlns:SOAPSDK1="http://www.w3.org/2001/XMLSchema" xmlns:SOAPSDK2="http://www.w3.org/2001/XMLSchema-instance" xmlns:SOAPSDK3="http://schemas.xmlsoap.org/soap/encoding/" xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/">
+		<SOAP-ENV:Header>
+		<SessionID>${sessionId}</SessionID>
+		</SOAP-ENV:Header>
+		<SOAP-ENV:Body>
+		<M1:GetTrafficMeterStatistics xmlns:M1="urn:NETGEAR-ROUTER:service:DeviceConfig:1"></M1:GetTrafficMeterStatistics>
+		</SOAP-ENV:Body>
 		</SOAP-ENV:Envelope>`;
 }
 
@@ -84,7 +124,12 @@ function getSoapHeader(action) {
 }
 
 function isValidResponse(resp) {
-	return (resp.statusCode === 200 && resp.body.includes('<ResponseCode>000</ResponseCode>'));
+	const validResponse = resp.statusCode === 200 && resp.body.includes('<ResponseCode>000</ResponseCode>');
+	if (!validResponse) {
+		console.log(resp.body);
+		console.log(resp.statusCode);
+	}
+	return (validResponse);
 }
 
 class NetgearRouter {
@@ -131,7 +176,7 @@ class NetgearRouter {
 				username: this.username,
 				password: this.password,
 			});
-			this.makeRequest(ACTION_LOGIN, message)
+			this.makeRequest(ACTION_LOGIN, message, false)
 			.then((result) => {
 				// console.log(result.statusCode);
 				console.log(result.body);
@@ -224,9 +269,9 @@ class NetgearRouter {
 							IP: entries[i].IP[0],																	// e.g. '10.0.0.10'
 							Name: entries[i].Name[0],															// '--' for unknown
 							NameUserSet: entries[i].NameUserSet[0] === 'true',		// e.g. 'false'
-							MAC: entries[i].NameUserSet[0],												// e.g. '61:56:FA:1B:E1:21'
+							MAC: entries[i].MAC[0],												// e.g. '61:56:FA:1B:E1:21'
 							ConnectionType: entries[i].ConnectionType[0],					// e.g. 'wired', '2.4GHz', 'Guest Wireless 2.4G'
-							SSID: entries[i].ConnectionType[0],										// e.g. 'MyWiFi'
+							SSID: entries[i].SSID[0],										// e.g. 'MyWiFi'
 							Linkspeed: entries[i].Linkspeed[0],
 							SignalStrength: Number(entries[i].SignalStrength[0]),	// number <= 100
 							AllowOrBlock: entries[i].AllowOrBlock[0],							// 'Allow' or 'Block'
@@ -252,64 +297,103 @@ class NetgearRouter {
 		// Resolves promise of traffic meter stats. Rejects if error occurred.
 		console.log('Get traffic meter');
 		return new Promise((resolve, reject) => {
-			const message = soapTrafficMeter({ sessionId: SESSION_ID });
+			const message = soapTrafficMeter(SESSION_ID);
 			this.makeRequest(ACTION_GET_TRAFFIC_METER,	message)
 			.then((result) => {
-				console.log(result.body);
+				// console.log(result.body);
 				const newTodayUpload = Number(REGEX_NEW_TODAY_UPLOAD.exec(result.body)[1]);
 				const newTodayDownload = Number(REGEX_NEW_TODAY_DOWNLOAD.exec(result.body)[1]);
 				const traffic = {	newTodayUpload, newTodayDownload };	// in Mbytes
 				resolve(traffic);
 			})
 			.catch((error) => {
-				console.log(error);
 				reject(Error(error));
 			});
 		});
 	}
 
-	makeRequest(action, message) {
+	// THIS FUNCTION IS NOT WORKING AT THE MOMENT!!!
+	setBlockDevice(MAC, AllowOrBlock) {
+		// Resolves promise of AllowOrBlock status. Rejects if error occurred.
+		console.log('setBlockDevice step 1 started');
 		return new Promise((resolve, reject) => {
-			if (!this.logged_in && action !== ACTION_LOGIN) {	// if not already loggedin, try to login first
-				console.log('not logged in, trying first time login now');
-				this.login()
-				.then((result) => {	// continue with request after successful login
-					if (this.logged_in) {	this.makeRequest(action, message); } else {
-						reject('not getting valid response or login fails');
-					}
-				})
-				.catch((error) => {
-					reject(error);	// reuest failed because login failed
-				});
-			}
-			// start with actual request here
-			const headers = getSoapHeader(action);
-			request.post({
-				url: this.soap_url,
-				headers,
-				data: message,
-			}, function (error, response, body) {
-				if (error) {
-					reject(error);
-					return;
-				}
-				this.logged_in = isValidResponse(response);
-				if (!this.logged_in && action !== ACTION_LOGIN) {	// try to login first if repsonse failed, but only once
-					console.log('no valid response, trying to login again');
-					this.login()
-					.then((result) => {	// continue with request after successful login
-						if (this.logged_in) {	this.makeRequest(action, message); } else {
-							reject('not getting valid response or login fails');
-						}
+			const message = soapConfigurationStarted(SESSION_ID);
+			this.makeRequest(ACTION_CONFIGURATION_STARTED,	message)
+			.then((result) => {
+				console.log('setBlockDevice step 2 started');
+				const mess = soapSetBlockDevice({ sessionId: SESSION_ID, MAC, AllowOrBlock });
+				this.makeRequest(ACTION_SET_BLOCK_DEVICE,	mess)
+					.then((res) => {
+						console.log('setBlockDevice step 3 can start');
+						resolve(res.body);
 					})
-					.catch((error) => {
-						reject(error);	// reuest failed because login failed
-					});
-				} else { resolve(response); }	// resolve the request
+				.catch((error) => {
+					reject(Error(error));
+				});
+			})
+			.catch((error) => {
+				reject(Error(error));
 			});
 		});
 	}
 
+	makeRequest(action, message, tryLoginAfterFailure) {
+		return new Promise((resolve, reject) => {
+			// first login if not logged_in or if previous action failed, but not if the action is actually to login :)
+			if (tryLoginAfterFailure === undefined) { tryLoginAfterFailure = true; }
+			if (tryLoginAfterFailure && !this.logged_in) {
+				console.log('not logged in or previous request failed, trying to login now');
+				this.login()
+				.then((result) => {	// continue with request after successful login
+					console.log(`first login succeeded: ${result}`);
+					if (this.logged_in) {
+						this.makeRequest(action, message, false)
+						.then((res) => {	// continue with request after retry
+							this.logged_in = isValidResponse(res);
+							if (isValidResponse(res)) {
+								resolve(res); 							// resolve the request
+							}	else { reject(res); }			// request failed
+						})
+						.catch((error) => {
+							reject(error);	// request failed
+						});
+					} else { reject('not getting valid response or login fails');	return;	} // request failed
+				})
+				.catch((error) => {
+					reject(error);	// reuest failed because login failed
+				});
+			} else {
+			// start with actual request here
+				const headers = getSoapHeader(action);
+				const thisParent = this;
+				request.post({
+					url: thisParent.soap_url,
+					headers,
+					data: message,
+				}, (error, response, body) => {
+					if (error) {
+						reject(error);
+						return;
+					}
+					const success = isValidResponse(response);
+					if (success) { resolve(response); return; } // resolve the request
+					console.log(`tryLAF: ${tryLoginAfterFailure}`);
+					if (!success && tryLoginAfterFailure) {	// one retry after fail
+						console.log('no valid response, trying again with new login');
+						thisParent.makeRequest(action, message, true)
+						.then((result) => {	// continue with request after retry
+							thisParent.logged_in = isValidResponse(result); // resolve the request
+							if (thisParent.logged_in) { resolve(result); } else { reject(result); }
+						})
+						.catch((error) => {
+							reject(error);	// request failed
+						});
+					} else { reject(response); } // request failed
+				});
+			}
+
+		});
+	}
 }
 
 module.exports = NetgearRouter;
